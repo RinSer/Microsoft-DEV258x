d6f3ed840da86e176162e1d77e7713578700d371
Serj <serj.dukareff@gmail.com>
2018-05-28 22:06:09 +0300

    Module 3 Lab 2 completed

--- Module1Lab3/ModuleHelpers/Module3Helper.cs
+++ Module1Lab3/ModuleHelpers/Module3Helper.cs
@@ -88,17 +88,172 @@ namespace MovieApp
 
         public static void ManyToManySelect()
         {
-            Console.WriteLine(nameof(ManyToManySelect));
+            // Ordered films with actors
+            var films = MoviesContext.Instance.Films
+                .OrderBy(f => f.Title)
+                .Include(f => f.FilmActor)
+                .ThenInclude(a => a.Actor);
+
+            foreach (var film in films)
+            {
+                Console.WriteLine(new string('-', 78));
+                Console.WriteLine($"{film.FilmId}\t{film.ReleaseYear}\t{film.Title}");
+                Console.WriteLine(new string('-', 78));
+                var actors = film.FilmActor.Select(a => a.Actor)
+                                    .OrderBy(a => a.LastName)
+                                    .ThenBy(a => a.FirstName);
+                foreach (var filmActor in film.FilmActor)
+                {
+                    Console.WriteLine($"\t{filmActor.ActorId}\t{filmActor.Actor.LastName}, {filmActor.Actor.FirstName}");
+                }
+            }
+            // Sorted actors with films
+            var actors2 = MoviesContext.Instance.Actors
+                .OrderBy(a => a.LastName)
+                .ThenBy(a => a.FirstName)
+                .Include(a => a.FilmActor)
+                .ThenInclude(f => f.Film);
+            foreach (var actor in actors2)
+            {
+                Console.WriteLine(new string('-', 78));
+                Console.WriteLine($"{actor.ActorId}\t{actor.LastName}, {actor.FirstName}");
+                Console.WriteLine(new string('-', 78));
+                var films2 = actor.FilmActor.Select(a => a.Film).OrderByDescending(f => f.ReleaseYear);
+                foreach (var film in films2)
+                {
+                    Console.WriteLine($"\t{film.FilmId}\t{film.ReleaseYear}\t{film.Title}");
+                }
+            }
         }
 
         public static void ManyToManyInsert()
         {
-            Console.WriteLine(nameof(ManyToManyInsert));
+            // Add actor to a film
+            var filmId = 12;
+            var film = MoviesContext.Instance.Films
+                            .Include(f => f.FilmActor)
+                            .ThenInclude(a => a.Actor)
+                            .Single(f => f.FilmId == filmId);
+            var actor = new Actor { FirstName = "Stan", LastName = "Lee" };
+
+            if (film.FilmActor.All(fa => fa.ActorId != actor.ActorId))
+            {
+                Console.WriteLine("Adding actor to film.");
+                film.FilmActor.Add(new FilmActor
+                {
+                    Film = film,
+                    Actor = actor
+                });
+            }
+            MoviesContext.Instance.SaveChanges();
+
+            film = MoviesContext.Instance.Films
+                            .Include(f => f.FilmActor)
+                            .ThenInclude(a => a.Actor)
+                            .Single(f => f.FilmId == filmId);
+
+            foreach (var filmActor in film.FilmActor)
+            {
+                Console.WriteLine($"{filmActor.ActorId}\t{filmActor.Actor.FirstName}\t{filmActor.Actor.LastName}");
+            }
+            // Add film to an actor
+            var actorId = 3;
+            filmId = 12;
+
+            actor = MoviesContext.Instance.Actors
+                            .Include(a => a.FilmActor)
+                            .ThenInclude(fa => fa.Film)
+                            .Single(a => a.ActorId == actorId);
+            if (actor.FilmActor.All(fa => fa.FilmId != filmId))
+            {
+                Console.WriteLine("Adding film to actor");
+                actor.FilmActor.Add(new FilmActor { Actor = actor, FilmId = filmId });
+                MoviesContext.Instance.SaveChanges();
+            }
+
+            actor = MoviesContext.Instance.Actors
+                            .Include(a => a.FilmActor)
+                            .ThenInclude(fa => fa.Film)
+                            .Single(a => a.ActorId == actorId);
+            foreach (var filmWithActor in actor.FilmActor.Select(fa => fa.Film))
+            {
+                Console.WriteLine($"{filmWithActor.FilmId}\t{filmWithActor.ReleaseYear}\t{filmWithActor.Title}");
+            }
         }
 
         public static void ManyToManyDelete()
         {
-            Console.WriteLine(nameof(ManyToManyDelete));
+            var filmActor = GetRandomFilmActor();
+            var filmId = filmActor.FilmId;
+            var actorId = filmActor.ActorId;
+
+            Write(filmActor);
+
+            var entity = new FilmActor
+            {
+                FilmId = filmId,
+                ActorId = actorId
+            };
+            MoviesContext.Instance.FilmActors.Remove(entity);
+            MoviesContext.Instance.SaveChanges();
+
+            filmActor = MoviesContext.Instance.FilmActors
+                            .SingleOrDefault(fa => fa.FilmId == filmId &&
+                                                    fa.ActorId == actorId);
+
+            Write(filmActor);
+
+            MoviesContext.Instance.FilmActors.Add(new FilmActor
+            {
+                FilmId = filmId,
+                ActorId = actorId
+            });
+            MoviesContext.Instance.SaveChanges();
+
+            filmActor = MoviesContext.Instance.FilmActors
+                            .SingleOrDefault(fa => fa.FilmId == filmId &&
+                                                    fa.ActorId == actorId);
+
+            Write(filmActor);
+        }
+
+        // Helper methods for many to many delete
+        private static void Write(FilmActor filmActor)
+        {
+            if (filmActor == null)
+            {
+                Console.WriteLine("Film Actor Not Found");
+                return;
+            }
+
+            var film = filmActor.Film;
+            var actor = filmActor.Actor;
+            if (film == null)
+            {
+                film = MoviesContext.Instance.Films
+                            .FirstOrDefault(f => f.FilmId == filmActor.FilmId);
+            }
+            if (actor == null)
+            {
+                actor = MoviesContext.Instance.Actors
+                            .FirstOrDefault(a => a.ActorId == filmActor.ActorId);
+            }
+
+            Console.WriteLine($"Film: {film.FilmId}  -  {film.Title}\t Actor: {actor.ActorId}  -  {actor.FirstName} {actor.LastName}");
+        }
+
+        private static FilmActor GetRandomFilmActor()
+        {
+            int count = MoviesContext.Instance.FilmActors.Count();
+            var skip = new Random().Next(0, count);
+            return MoviesContext.Instance.FilmActors
+                        .Skip(skip)
+                        .Select(fa => new FilmActor
+                        {
+                            FilmId = fa.FilmId,
+                            ActorId = fa.ActorId
+                        })
+                        .First();
         }
     }
 }
